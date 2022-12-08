2483db256634e5b4de998f59867faa847eca7de7
aSimpleUserOrSomeone <37453773+aSimpleUserOrSomeone@users.noreply.github.com>
2022-12-08 08:09:50 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Sudoku from './Sudoku.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<html lang="en">
+<h1>Play Sudoku!</h1>
+	<Sudoku/>
+</html>
+
+<style>
+
+	h1 {
+		width: 100%;
+	}
+
+</style>
---
+++ src/routes/Sudoku.svelte
@@ -0,0 +1,25 @@
+<script>
+	console.log('Page Load')
+	//https://rapidapi.com/gregor-i/api/sudoku-generator1
+
+	const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+		'X-RapidAPI-Host': 'sudoku-generator1.p.rapidapi.com'
+	}
+};
+
+fetch('https://sudoku-generator1.p.rapidapi.com/sudoku/generate?seed=1337', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+</script>
+
+<div >
+
+</div>
+
+<style>
+
+</style>
