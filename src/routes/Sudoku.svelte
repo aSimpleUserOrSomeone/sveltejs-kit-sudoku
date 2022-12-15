40b5805bb91623f1605827dbe3ef8ada0f50d594
aSimpleUserOrSomeone <37453773+aSimpleUserOrSomeone@users.noreply.github.com>
2022-12-15 08:07:33 +0100

    Local json tomfoolery

--- src/routes/Sudoku.svelte
+++ src/routes/Sudoku.svelte
@@ -1,19 +1,8 @@
 <script>
 	console.log('Page Load')
-	//https://rapidapi.com/gregor-i/api/sudoku-generator1
 
-	const options = {
-		method: 'GET',
-		headers: {
-			'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-			'X-RapidAPI-Host': 'sudoku-generator1.p.rapidapi.com'
-		}
-	};	
-
-	fetch('https://sudoku-generator1.p.rapidapi.com/sudoku/generate?seed=1337', options)
-		.then(response => response.json())
-		.then(response => console.log(response))
-		.catch(err => console.error(err));
+	fetch("../../static/sudokuData.json")
+	.then(res => console.log(res))
 
 	const sudoku = {
 		seed:1337,
